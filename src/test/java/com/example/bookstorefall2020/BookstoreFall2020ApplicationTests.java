faf23cf5771e784a910c8d8215942ab41f008423
OsLii <liimatta.oskari@gmail.com>
2020-10-25 14:14:51 +0200

    pom xml

--- src/test/java/com/example/bookstorefall2020/BookstoreFall2020ApplicationTests.java
+++ src/test/java/com/example/bookstorefall2020/BookstoreFall2020ApplicationTests.java
@@ -2,15 +2,14 @@ package com.example.bookstorefall2020;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import com.example.bookstorefall2020.web.BookController;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookstoreFall2020ApplicationTests {
 
